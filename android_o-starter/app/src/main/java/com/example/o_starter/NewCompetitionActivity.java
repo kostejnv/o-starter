dc1c413835e2206bc89bdf07e979892f70acce84
kostejnv <v.kostejn@seznam.cz>
2021-05-03 20:44:10 +0200

    add unstarted runner and changed runner as table to database

---
+++ android_o-starter/app/src/main/java/com/example/o_starter/NewCompetitionActivity.java
@@ -0,0 +1,14 @@
+package com.example.o_starter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewCompetitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_competition);
+    }
+}
