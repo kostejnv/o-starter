4ddb1365e7a6b2e64b684670df6b1094204d287d
kostejnv <v.kostejn@seznam.cz>
2021-05-31 22:37:38 +0200

    server docs added

--- server_o-starter/o_starter/startlists/admin.py
+++ server_o-starter/o_starter/startlists/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from .models import Race, Change, Unstarted_runner
 
+# Register your models here.
 admin.site.register(Race)
 admin.site.register(Change)
 admin.site.register(Unstarted_runner)
-# Register your models here.
 
